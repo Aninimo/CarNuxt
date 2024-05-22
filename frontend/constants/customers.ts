e63a18121c94130eae4d1d6e26f0c7a4795342b4
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-05-22 07:28:15 -0300

    Create customers.ts

---
+++ frontend/constants/customers.ts
@@ -0,0 +1,35 @@
+export const customers = [
+  {
+    id: "1",
+    image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "John Doe",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu velit quis mauris imperdiet cursus.",
+  },
+
+  {
+    id: "2",
+    image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "John Doe",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu velit quis mauris imperdiet cursus.",
+  },
+
+  {
+    id: "3",
+    image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "John Doe",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu velit quis mauris imperdiet cursus.",
+  },
+
+  {
+    id: "4",
+    image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "John Doe",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu velit quis mauris imperdiet cursus.",
+  },
+  {
+    id: "5",
+    image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "John Doe",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu velit quis mauris imperdiet cursus.",
+  },
+]
